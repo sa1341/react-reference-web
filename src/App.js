add643823737bab1c4ed2ed60fef254713d4f990
임준영 <a79007714@gmail.com>
2021-01-25 23:34:33 +0900

    feat: TodoList를 작성하면서 프로젝트 전역의 상태나 값을 관리하는 Context 개념에 대해서 공부하였습니다. Context는 내부적으로 Provider라는 컴포넌트를 가지고 있고 Provider는 props로 state, dispatch 등 여러 값들을 지정하여 가질 수 있고, 하위 컴포넌트에서 해당 Context를 조회하여 해당 값을 사용할 수 있습니다. 이렇게 하면 리액트 트리구조에서 계층간의 props나 함수들을 전달받을 필요 없이 컨텍스트에서 직접 상태 값을 가져오고 변경하므로써 규모가 큰 프로젝트 환경에서 더 유연하게 대처가 가능하다는 장점이 있다는 것을 알게 되었습니다.

--- src/App.js
+++ src/App.js
@@ -1,47 +1,33 @@
 import LoginForm from './components/LoginForm';
-import styled from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 import { Route, Link } from 'react-router-dom';
-import Home from './components/Home';
-import Profiles from './components/Profiles';
-import About from './components/About';
-import HistorySample from './components/HistorySample'; 
-
-
+import TodoTemplate from './components/TodoTemplate';
+import TodoHead from './components/TodoHead';
+import TodoList from './components/TodoList';
+import TodoCreate from './components/TodoCreate';
+import { TodoProvider } from './components/TodoContext';
 
 const App = () => {
 
-  const Circle = styled.div`
-    width: 5rem;
-    height: 5rem;
-    background: ${props => props.color || 'black'};
-    border-radius: 50%;
+  
+  const GlobalStyle = createGlobalStyle`
+    body {
+      background: #e9ecef;
+    }
   `;
 
 
-
   return (
-    <div>
-       <ul>
-        <li>
-          <Link to="/">홈</Link>
-        </li>
-        <li>
-          <Link to="/loginForm">로그인 폼</Link>
-        </li>
-        <li>
-          <Link to="/profiles">프로필 목록</Link>
-        </li>
-        <li>
-          <Link to="/history">예제</Link>
-        </li>
-      </ul>
-      <hr />
-      <Route path="/" exact={true} component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/loginForm" component={LoginForm} /> 
-      <Route path="/profiles" component={Profiles} />
-      <Route path="/history" component={HistorySample} />
-    </div>
+    <>
+    <TodoProvider>
+      <GlobalStyle/>
+      <TodoTemplate>
+        <TodoHead/>
+        <TodoList/>
+        <TodoCreate/>
+      </TodoTemplate>
+    </TodoProvider>
+    </>
   );
 };
 
